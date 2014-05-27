a7dc1cfd374a319340616929a7de522b09a03ba7
Marin Atanasov Nikolov <dnaeon@gmail.com>
2014-05-27 17:12:02 +0300

    Commit setup.py file to the repo

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='vevents-cli',
+      version='0.1.0',
+      description='Viewing VMware vSphere Events from the command-line',
+      author='Marin Atanasov Nikolov',
+      author_email='dnaeon@gmail.com',
+      license='BSD',
+      scripts=[
+        'src/vevents-cli',
+      ],
+      install_requires=[
+        'pyvmomi >= 5.5.0',
+        'docopt >= 0.6.1',
+        'vconnector >= 0.2.0',  
+      ]
+)
